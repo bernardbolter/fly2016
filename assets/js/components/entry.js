d2b13b3ba267cf310734b25a96b5978d1a4d3e08
Bernard Bolter <bemajor@bernards-imac.gateway.sonic.net>
2016-09-24 13:13:59 -0700

    refactored old site design to new workflow

--- assets/js/components/entry.js
+++ assets/js/components/entry.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Component } from 'react';
 
-import Logo from './logo/logo';
 
 export default class Entry extends Component {
   render() {
     return (
       <div className="layout">
-        <Logo />
+
       </div>
     );
   }
